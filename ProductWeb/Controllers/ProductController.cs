38b96d6f5bfc11a00e7e2390f36d33e246e6bd43
USER <USER@OLASILE>
2023-05-30 11:19:15 +0100

    ProductDemo for MicroService

---
+++ ProductWeb/Controllers/ProductController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using ProductWeb.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProductWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductContext _context;
+
+        public ProductController(ProductContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<ProductController>
+        [HttpGet]
+        public async Task<IActionResult> GetAllProduct()
+        {
+            var product =await _context.Products.ToListAsync();
+            return Ok(product);
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = _context.Products.FindAsync(id);
+            return Ok(product);
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct ([FromBody] Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return Ok();
+
+        }
+    }
+}
